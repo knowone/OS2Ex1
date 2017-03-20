a21df2465f57314785fe91a538cc9b1082350484
Omer Schwartz <knowone.omer@gmail.com>
2017-03-20 18:44:04 +0200

    Working thread skeleton file with thread return value

--- ex1a.c
+++ ex1a.c
@@ -7,19 +7,21 @@
 #define THREADS_NUM 6
 
 void * doWork(void*);
-void * change_arr(void*);
-void * read_arr(void*);
+void * change_arr(int*);
+void * read_arr(int*);
 
 int main() {
 
     pthread_t thread_id[THREADS_NUM];
-    int answers [THREADS_NUM];
-    memset(answers, 0, THREADS_NUM);
+    int thread_arr[THREADS_NUM];
+    //int * thread_answer;
+    //memset(thread_answer, 0, THREADS_NUM);
 
     int shd_arr[SIZE_ARR];
     memset(shd_arr,0,SIZE_ARR);
     for (int i = 0; i < THREADS_NUM; ++i) {
-        int status = pthread_create(&thread_id[i],NULL,(void*)doWork,&i);
+        thread_arr[i] = i;
+        int status = pthread_create(&thread_id[i],NULL,(void*)doWork,&thread_arr[i]);
         if (status != 0){
             fputs("Can't create pthread in main()", stderr);
             exit(EXIT_FAILURE);
@@ -27,20 +29,34 @@ int main() {
     }
 
     for (int i = 0; i < THREADS_NUM; ++i) {
-        pthread_join(thread_id[i],(void**) &answers);
-        printf("thread %d says: %d", i+1,answers[i]);
+        int * thread_answer = (int *) malloc(sizeof(int));
+        pthread_join(thread_id[i],(void**) &thread_answer);
+        //Here be thread answer int
+        printf("thread %d says: %d\n", i+1,*(thread_answer));
     }
 
     return 0;
 }
 
 void * doWork(void* args){
-    int i = *((int*)args);
-    if (i >=THREADS_NUM/2 && i < THREADS_NUM){
-        change_arr(NULL);
-        pthread_exit()
+    int* thread_number = (int*)args;
+    if (*(thread_number) >=THREADS_NUM/2 && *(thread_number) < THREADS_NUM){
+        change_arr(thread_number);
     }
     else{
-        return read_arr(NULL);
+        read_arr(thread_number);
+        printf("%d in thread\n", *thread_number);
     }
+    //Next line is unused
+    pthread_exit(thread_number);
+}
+
+void * read_arr(int * thread_ans){
+    *thread_ans = 2;
+    return NULL;
+}
+
+void * change_arr(int* nop){
+    *(nop) = 1;
+    pthread_exit((void*)nop);
 }
