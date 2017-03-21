03528ba369081adcd59c4ebff37597e8a7a5bc5b
Omer Schwartz <knowone.omer@gmail.com>
2017-03-21 18:25:12 +0200

    Changed method of data storage. Currently faults with SIGSEGV + cleanup

--- ex1a.c
+++ ex1a.c
@@ -1,71 +1,69 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 #define SIZE_ARR 100
 #define THREADS_NUM 6
 
 void * doWork(void*);
-void * change_arr(int*);
-void * read_arr(int*);
-void clean_mem(void *);
+void * change_arr(void*);
+void * read_arr(void*);
+
+typedef struct{
+    int thread_num;
+    int thread_answer;
+}thread_data_t;
 
 int main() {
-
+    srand((unsigned int)time(NULL));
     pthread_t thread_id[THREADS_NUM];
-    int thread_arr[THREADS_NUM];
-    //int * thread_answer;
-    //memset(thread_answer, 0, THREADS_NUM);
+    thread_data_t thread_data[THREADS_NUM];
 
-    int shd_arr[SIZE_ARR];
-    memset(shd_arr,0,SIZE_ARR);
     for (int i = 0; i < THREADS_NUM; ++i) {
-        thread_arr[i] = i;
-        int status = pthread_create(&thread_id[i],NULL,(void*)doWork,&thread_arr[i]);
+        thread_data[i].thread_num = i;
+        int status = pthread_create(&thread_id[i],NULL,(void*)doWork,&thread_data[i]);
         if (status != 0){
             fputs("Can't create pthread in main()", stderr);
             exit(EXIT_FAILURE);
         }
     }
-    int * thread_answer = (int *) malloc(sizeof(int));
-    if (thread_answer == NULL){
-        perror("Allocating memory failed. main()");
-        exit(EXIT_FAILURE);
-    }
-    pthread_cleanup_push(clean_mem, thread_answer);
+
     for (int i = 0; i < THREADS_NUM; ++i) {
-        pthread_join(thread_id[i],(void**) &thread_answer);
+        pthread_join(thread_id[i],(void**) &(thread_data[i].thread_answer)+sizeof(int*));
         //Here be thread answer int
-        printf("thread %d says: %d\n", i+1,*(thread_answer));
+        printf("thread %d says: %d\n", i+1,thread_data[i].thread_answer);
     }
     pthread_exit(NULL);
-    pthread_cleanup_pop(0);
 }
 
 void * doWork(void* args){
-    int* thread_number = (int*)args;
-    if (*(thread_number) >=THREADS_NUM/2 && *(thread_number) < THREADS_NUM){
-        change_arr(thread_number);
+
+    thread_data_t * td = (thread_data_t*)args;
+    if (td->thread_num >=THREADS_NUM/2 && td->thread_num < THREADS_NUM){
+        change_arr(td);
     }
     else{
-        read_arr(thread_number);
-        printf("%d in thread\n", *thread_number);
+        read_arr(td);
+        printf("in thread %d\n", td->thread_num);
     }
     //Next line is unused
-    pthread_exit(thread_number);
+    pthread_exit(td);
+//    pthread_cleanup_pop(0);
+
 }
 
-void * read_arr(int * thread_ans){
-    *thread_ans = 2;
-    return NULL;
+void * read_arr(void * nop){
+
+   sleep(((unsigned int)rand()%3+1));
+    thread_data_t * tmp = (thread_data_t*)nop;
+    tmp->thread_answer = 1;
+    pthread_exit((void*)tmp);
 }
 
-void * change_arr(int* nop){
-    *(nop) = 1;
-    pthread_exit((void*)nop);
-}
-
-void clean_mem(void * mem){
-    free(mem);
+void * change_arr(void * nop){
+    thread_data_t * tmp = (thread_data_t*)nop;
+    tmp->thread_answer = 2;
+    pthread_exit((void*)tmp);
 }
